92be3fac6f99e6546a51d459d1883ed774bbc05b
Eduardo Lemos Rocha <edu@localhost.localdomain>
2019-05-03 15:10:49 -0300

    Google integrado

--- lib/cadastro/login.dart
+++ lib/cadastro/login.dart
@@ -4,6 +4,11 @@ import 'package:flutter/services.dart';
 import 'cadastro_pessoal_elementos.dart';
 import '../icons/custom_icons.dart';
 import '../common.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
 
 class LoginPage extends StatelessWidget {
   var scaffoldKey = GlobalKey<ScaffoldState>();
@@ -73,14 +78,7 @@ class LoginFormState extends State<LoginForm> {
             ),
             onPressed:() {Navigator.pop(context);},
           ),
-          FlatButton(
-            child: FaceGoogleCont(
-                text: TextFaceGoogle("  ENTRAR COM GOOGLE"),
-                color: 0xfff15f5c,
-                icon: GoogleIcon(),
-            ),
-            onPressed:() {Navigator.pop(context);},
-          ),
+          _GoogleSignInSection(),  
         ],
       ),
     );
@@ -154,5 +152,76 @@ class GoogleIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Icon(CustomIcons.gplus, color: Color(0xfff7f7f7), size: 13,);
+  }
+}
+
+class _GoogleSignInSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _GoogleSignInSectionState();
+}
+
+class _GoogleSignInSectionState extends State<_GoogleSignInSection> {
+  bool _success;
+  String _userID;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: const Text('Test sign in with Google'),
+          padding: const EdgeInsets.all(16),
+          alignment: Alignment.center,
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _signInWithGoogle();
+            },
+            child: const Text('Sign in with Google'),
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            _success == null
+                ? ''
+                : (_success
+                    ? 'Successfully signed in, uid: ' + _userID
+                    : 'Sign in failed'),
+            style: TextStyle(color: Colors.red),
+          ),
+        )
+      ],
+    );
+  }
+
+  // Example code of how to sign in with google.
+  void _signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    final FirebaseUser user = await _auth.signInWithCredential(credential);
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        _success = true;
+        _userID = user.uid;
+      } else {
+        _success = false;
+      }
+    });
   }
 }
